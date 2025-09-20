b70c70108bf1e694672b57183a812bbf664e1a0b
thurtz <44620177+thurtz@users.noreply.github.com>
2025-09-20 09:50:34 -0700

    feat(docs): Configure Sphinx for autodoc and RTD theme
    - Add Sphinx and sphinx_rtd_theme to development requirements.
    - Configure docs/source/conf.py to:
      - Add the project root to sys.path.
      - Enable sphinx.ext.autodoc, sphinx.ext.napoleon, and sphinx.ext.viewcode extensions.
      - Set the HTML theme to sphinx_rtd_theme.

---
+++ docs/source/conf.py
@@ -0,0 +1,38 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'micro_X'
+copyright = '2025, micro_X Team'
+author = 'micro_X Team'
+
+version = '1.0'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
