16acd5755f08893a51ba1cd25b1cf58adff0a923
thurtz <44620177+thurtz@users.noreply.github.com>
2026-03-06 17:57:56 -0800

    fix: Add automatic worktree prune to clone utility
    - Added 'git worktree prune' to utils/clone.py before creating new clones.
    
    - Updated tests/test_clone_utility.py to account for the extra git call.
    
    - Bumped version to 0.0.1056.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,8 +14,8 @@ project = 'micro_X'
 copyright = '2025, micro_X Team'
 author = 'micro_X Team'
 
-version = '0.0.1055'
-release = '0.0.1055'
+version = '0.0.1056'
+release = '0.0.1056'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
